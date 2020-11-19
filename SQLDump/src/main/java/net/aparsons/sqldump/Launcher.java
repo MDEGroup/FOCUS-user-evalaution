ad4b7a86ae366cbf149205e68e9138d6699ccf44
Matteo Bosco <matteobosco@alice.it>
2020-11-19 18:56:17 +0100

    evaluation done

--- SQLDump/src/main/java/net/aparsons/sqldump/Launcher.java
+++ SQLDump/src/main/java/net/aparsons/sqldump/Launcher.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import net.aparsons.sqldump.db.Connectors.Connector;
@@ -36,11 +37,41 @@ public class Launcher {
 		
 		 
 		final Option urlOption = new Option("url", true, "A database url of the form jdbc:subprotocol:subname");
+		final Option usernameOption = new Option("user", true, "Username for database");
+		final Option passwordOption = new Option("pass", true, "Password for database");
+		final Option queryOption = new Option("sql", true, "Query to execute");
+		final Option csvPathOption = new Option("f", "file", true, "CSV File path");
+		final Option includeHeaderOption = new Option("headers", false, "Include headers");
 		
 		final OptionGroup urlGroup = new OptionGroup();
 		urlGroup.setRequired(true);
 		urlGroup.addOption(urlOption);
 		options.addOptionGroup(urlGroup);
+		
+		final OptionGroup usernameGroup = new OptionGroup();
+		usernameGroup.setRequired(true);
+		usernameGroup.addOption(usernameOption);
+		options.addOptionGroup(usernameGroup);
+		
+		final OptionGroup passwordGroup = new OptionGroup();
+		passwordGroup.setRequired(true);
+		passwordGroup.addOption(passwordOption);
+		options.addOptionGroup(passwordGroup);
+		
+		final OptionGroup queryGroup = new OptionGroup();
+		queryGroup.setRequired(true);
+		queryGroup.addOption(queryOption);
+		options.addOptionGroup(queryGroup);
+		
+		final OptionGroup csvPathGroup = new OptionGroup();
+		csvPathGroup.setRequired(false);
+		csvPathGroup.addOption(csvPathOption);
+		options.addOptionGroup(csvPathGroup);
+		
+		final OptionGroup includeHeaderGroup = new OptionGroup();
+		includeHeaderGroup.setRequired(false);
+		includeHeaderGroup.addOption(includeHeaderOption);
+		options.addOptionGroup(includeHeaderGroup);
 		
 		//COMPLETE THE METHOD
 		return options;
@@ -50,11 +81,25 @@ public class Launcher {
 	 * Prints the command line options to the console and return print usage as string
 	 */
 	public static String printUsage() {
-		HelpFormatter printer = new HelpFormatter();
-		
-		printer.printHelp("Help", getOptions());
-		//COMPLETE THE METHOD
-		return "";
+		//HelpFormatter printer = new HelpFormatter();
+		//printer.printHelp("Help", getOptions());
+		Iterator ir = getOptions().getOptions().iterator();
+		String usage = "\nHelp:\n\nList of arguments and usage\n\n";
+		while(ir.hasNext()) {
+			Option o = (Option) ir.next();
+			String longOpt = "";
+			String arg = "";
+			String desc = o.getDescription();
+			String opt = o.getOpt();
+			if(o.hasArg()) {
+				arg = "<arg>";
+			}
+			if(o.getLongOpt() != null) {
+				longOpt = ", --" + o.getLongOpt();
+			}
+			usage += "-" + opt + longOpt + " " + arg + "\t\t" + desc + "\n";
+		}
+		return usage;
 	}
 
 	
@@ -71,14 +116,39 @@ public class Launcher {
 		try {
 			CommandLineParser parser = new GnuParser();
 			CommandLine cmdLine = parser.parse(getOptions(), consoleParams);
-			if(!cmdLine.hasOption("url")) throw new ParseException("No url is specifified");
+			
+			if(!cmdLine.hasOption("url")) throw new ParseException("No url is specified");
 			String url = cmdLine.getOptionValue("url");
 			
-			//COMPLETE THE METHOD
+			if(!cmdLine.hasOption("user")) throw new ParseException("Username required");
+			String username = cmdLine.getOptionValue("user");
+			
+			if(!cmdLine.hasOption("pass")) throw new ParseException("Password required");
+			String password = cmdLine.getOptionValue("pass");
+			
+			if(!cmdLine.hasOption("sql")) throw new ParseException("No query provided");
+			String query = cmdLine.getOptionValue("sql");
+			
+			result.put("url", url);
+			result.put("username", username);
+			result.put("password", password);
+			result.put("sql", query);
+			
+			if(cmdLine.hasOption("f")) {
+				result.put("file", cmdLine.getOptionValue("f"));
+			}
+			
+			if(cmdLine.hasOption("file")) {
+				result.put("file", cmdLine.getOptionValue("file"));
+			}
+			
+			if(cmdLine.hasOption("headers")) {
+				result.put("headers", cmdLine.getOptionValue("headers"));
+			}
 			
 			
 		} catch (ParseException pe) {
-			System.out.println(printUsage());
+			//System.out.println("\n"+pe.getMessage()+"\n");
 			throw pe;
 		}
 		return result;
@@ -108,7 +178,7 @@ public class Launcher {
 			businessLogic(param.get("protocol"), param.get("url"), 
 					param.get("username"), param.get("password"), param.get("sql"), param.get("file"), false);
 		} catch (ParseException pe) {
-			printUsage();
+			System.out.println(printUsage());
 		}
 	}
 
